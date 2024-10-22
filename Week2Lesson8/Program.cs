26160e28851e6f8e5d70f5bbdbda84e5b6c66b4e
mrelidzinski <maciek220398@interia.pl>
2024-10-22 22:07:28 +0200

    Add project files.

---
+++ Week2Lesson8/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Task1
+        //int a;
+
+        //for (int i = 2; i <= 100; i++)
+        //{
+        //    for (int j = 2; j <= 100; j++)
+        //    {
+        //        if (j < i)                          // liczbe sprawdzana dzielimy jedynie przez liczby mniejsze od niej samej
+        //        {
+        //            a = i % j;
+        //            if (a == 0)                     // liczba ma dzielnik, nie jest wiec pierwsza; koniec petli;
+        //            {
+        //                j = 101;
+        //            }
+        //        }
+        //        else if (j == i)                    // liczba nie podzielila sie przez zadna inna, jest wiec liczba pierwsza
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //        else if (j > i)                     // koniec petli jesli dzielnik jest wiekszy od liczby sprawdzanej
+        //        {
+        //            j = 101;
+        //        }
+        //    }
+        //}
+
+
+        //Task2
+        //int b = 0;
+        //int i = 1;
+
+        //do
+        //{
+        //    b = i % 2;
+        //    if (b == 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //    i++;
+        //}
+        //while (i <= 1000);
+
+
+        //Task3
+        //int c = 0;
+        //int j = 1;
+        //int suma = 0;
+
+        //Console.WriteLine(suma);
+        //Console.WriteLine(j);
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    suma = j + c;
+        //    Console.WriteLine(suma);
+        //    c = j;
+        //    j = suma; ;
+        //}
+
+
+        //Task4
+
+    }
+}
